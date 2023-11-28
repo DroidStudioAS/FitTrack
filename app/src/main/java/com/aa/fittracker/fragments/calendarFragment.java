163174379c0cf208d7085d26b5c42560652e4734
DroidStudioAS <as.droiddevteam@gmail.com>
2023-11-28 07:04:12 +0100

    implemented initial calendar icons, now they just have to actively refresh, pushing to main

--- app/src/main/java/com/aa/fittracker/fragments/calendarFragment.java
+++ app/src/main/java/com/aa/fittracker/fragments/calendarFragment.java
@@ -19,6 +19,7 @@ import com.aa.fittracker.logic.DateParser;
 import com.aa.fittracker.logic.store;
 import com.aa.fittracker.models.TrainingEntry;
 import com.aa.fittracker.models.WeightEntry;
+import com.aa.fittracker.presentation.CalendarAdapter;
 import com.applandeo.materialcalendarview.CalendarDay;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
@@ -51,30 +52,15 @@ CalendarView calendarView;
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
         //fragment logic start
-        List<EventDay> list = new ArrayList<>();
-
-        //in u want to set the 11. month, u have to set 10.
-        //december is 11
-        //january is 0
-        //must adjust with -1
-
-        //store.setDateStrings("2023 10 10");
-        //store.setDateStrings("2023 11 11");
-        //store.setDateStrings("2023 12 12");
-
-
-       // calendarDay.setBackgroundResource(R.drawable.fit_track_icon); // Replace R.drawable.your_background_resource with your drawable resource
-        //   calendarDay.setBackgroundDrawable(yourDrawable); // Set your drawable here
-
-        //   calendarDay.setSelectedLabelColor(getResources().getColor(R.color.selectedColor)); // Replace R.color.selectedColor with your selected color resource
-        //  calendarDay.setSelectedBackgroundResource(R.drawable.selected_background_resource); // Replace R.drawable.selected_background_resource with your selected drawable resource
-        //  calendarDay.setSelectedBackgroundDrawable(yourSelectedDrawable); // Set your selected drawable here
-
-        // Add the configured CalendarDay object to the list
-
-
         // Set the list of CalendarDay objects to your calendarView
         calendarView=(CalendarView)view.findViewById(R.id.calendarView);
+
+        if (store.getTrainingEntries().isEmpty()){
+            Log.i("NO ENTRIES FETCHED YER","...");
+        }
+
+        CalendarAdapter.listReturner();
+
         calendarView.setOnDayClickListener(new OnDayClickListener() {
             @Override
             public void onDayClick(@NonNull EventDay eventDay) {
@@ -84,8 +70,6 @@ CalendarView calendarView;
                 int day = calendar.get(Calendar.DAY_OF_MONTH);
                 String date = year + "-" + month + "-"+ day;
                 store.setDateInFocus(date);
-
-
 
                 /************callback to activity**********/
                 Log.i("date in focus", store.getDateInFocus());
@@ -110,53 +94,7 @@ CalendarView calendarView;
             }
         });
        //fragment logic end
-        while (store.getTrainingEntries().isEmpty()){
-            Log.i("NO ENTRIES FETCHED YER","...");
-        }
-        switch (store.getUserMode()){
-            case "journal":
-                list.clear();
-                for(TrainingEntry x : store.getTrainingEntries()){
-                    Log.i("ACTIVE","ACTIVE");
-                    Calendar calToSet = Calendar.getInstance();
-                    String [] dateBits = x.getTraining_date().split("-");
-
-                    int monthWithoutCorrection = Integer.parseInt(dateBits[1]);
-                    int month = monthWithoutCorrection-1;
-
-                    calToSet.set(Calendar.YEAR, Integer.parseInt(dateBits[0]));
-                    calToSet.set(Calendar.MONTH, month);
-                    calToSet.set(Calendar.DAY_OF_MONTH,Integer.parseInt(dateBits[2]));
-
-                    list.add(new EventDay(calToSet,R.drawable.training_service_drawable));
-                    Log.i("event day size",String.valueOf(list.size()));
-
-                }
-                break;
-            case "weight":
-                for(WeightEntry x : store.getWeightEntries()){
-                    list.clear();
-                    Log.i("ACTIVE","ACTIVE");
-                    Calendar calToSet = Calendar.getInstance();
-                    String [] dateBits = x.getWeight_date().split("-");
-
-                    int monthWithoutCorrection = Integer.parseInt(dateBits[1]);
-                    int month = monthWithoutCorrection-1;
-
-                    calToSet.set(Calendar.YEAR, Integer.parseInt(dateBits[0]));
-                    calToSet.set(Calendar.MONTH, month);
-                    calToSet.set(Calendar.DAY_OF_MONTH,Integer.parseInt(dateBits[2]));
-
-                    list.add(new EventDay(calToSet,R.drawable.weight_service_drawable));
-                    Log.i("event day size",String.valueOf(list.size()));
-
-                }
-
-                break;
-        }
-
-
-        calendarView.setEvents(list);
+        calendarView.setEvents(store.getUserEventDaysActive());
         return view;
     }
 }
--- app/src/main/java/com/aa/fittracker/presentation/CalendarAdapter.java
+++ app/src/main/java/com/aa/fittracker/presentation/CalendarAdapter.java
@@ -4,23 +4,81 @@ import android.util.Log;
 
 import com.aa.fittracker.*;
 import com.aa.fittracker.logic.store;
+import com.aa.fittracker.models.TrainingEntry;
+import com.aa.fittracker.models.WeightEntry;
 import com.applandeo.materialcalendarview.EventDay;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 //Log.i("EVENTDAY :", store.getUserEventDaysActive)
 public class CalendarAdapter {
-    public static List<EventDay> listReturner(){
+    public static void listReturner(){
         Log.i("EVENT-DAY BEFORE CLR :", String.valueOf(store.getUserEventDaysActive().size()));
         store.getUserEventDaysActive().clear();
         Log.i("EVENT-DAY AFTER CLR :", String.valueOf(store.getUserEventDaysActive().size()));
-        List<EventDay> output = new ArrayList<>();
-        switch (store.getUserMode()){
 
+        List<EventDay> output = new ArrayList<>();
+
+        switch (store.getUserMode()){
+            case"journal":
+                for(TrainingEntry x : store.getTrainingEntries()){
+
+                    Calendar calendar = Calendar.getInstance();
+
+                   String[] parsed = x.getTraining_date().split("-");
+                   int year=Integer.parseInt(parsed[0]);
+                   int monthWithoutCorrection = Integer.parseInt(parsed[1]);
+                   int month = monthWithoutCorrection-1;
+                   int day= Integer.parseInt(parsed[2]);
+
+                   Log.i(" NCR DATE TO INSERT",String.valueOf(year + " " + monthWithoutCorrection + " " + day));
+                   Log.i(" CR DATE TO INSERT",String.valueOf(year + " " + month + " " + day));
+
+                   calendar.set(year,month,day);
+                   Log.i("CAL YEAR", String.valueOf(calendar.get(Calendar.YEAR)));
+                    Log.i("CAL MONTH", String.valueOf(calendar.get(Calendar.MONTH)));
+                    Log.i("CAL DAY", String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)));
+
+                    output.add(new EventDay(calendar,R.drawable.training_service_drawable));
+
+                }
+                for(EventDay x : output){
+                    Log.i("USER MODE", store.getUserMode());
+                    Log.i("LIST AFTER FOR-EACH", String.valueOf(x.getCalendar().get(Calendar.YEAR)) + " " + x.getCalendar().get(Calendar.MONTH) + " " + x.getCalendar().get(Calendar.DAY_OF_MONTH));
+                }
+                break;
+            case "weight":
+                for(WeightEntry x : store.getWeightEntries()){
+
+                    Calendar calendar = Calendar.getInstance();
+
+                    String[] parsed = x.getWeight_date().split("-");
+                    int year=Integer.parseInt(parsed[0]);
+                    int monthWithoutCorrection = Integer.parseInt(parsed[1]);
+                    int month = monthWithoutCorrection-1;
+                    int day= Integer.parseInt(parsed[2]);
+
+                    Log.i(" NCR DATE TO INSERT",String.valueOf(year + " " + monthWithoutCorrection + " " + day));
+                    Log.i(" CR DATE TO INSERT",String.valueOf(year + " " + month + " " + day));
+
+                    calendar.set(year,month,day);
+                    Log.i("CAL YEAR", String.valueOf(calendar.get(Calendar.YEAR)));
+                    Log.i("CAL MONTH", String.valueOf(calendar.get(Calendar.MONTH)));
+                    Log.i("CAL DAY", String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)));
+
+                    output.add(new EventDay(calendar,R.drawable.training_service_drawable));
+
+                }
+
+                for(EventDay x : output){
+                    Log.i("USER MODE", store.getUserMode());
+                    Log.i("LIST BEFORE FOR-EACH", String.valueOf(x.getCalendar().get(Calendar.YEAR)) + " " + x.getCalendar().get(Calendar.MONTH) + " " + x.getCalendar().get(Calendar.DAY_OF_MONTH));
+                }
+                break;
         }
 
-
-        return output;
+        store.setUserEventDaysActive(output);
     }
 
 
