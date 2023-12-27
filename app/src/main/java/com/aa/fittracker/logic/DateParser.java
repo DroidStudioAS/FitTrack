8f19b82d5d85e0e34d23b7e5fc81d32936eed966
DroidStudioAS <as.droiddevteam@gmail.com>
2023-12-27 10:27:17 +0100

    bottom fragment now always displays last 7 dates

--- app/src/main/java/com/aa/fittracker/logic/DateParser.java
+++ app/src/main/java/com/aa/fittracker/logic/DateParser.java
@@ -2,6 +2,7 @@ package com.aa.fittracker.logic;
 
 import android.util.Log;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class DateParser {
@@ -13,21 +14,36 @@ public class DateParser {
         return parsed;
     }
 
-    public static ArrayList<Integer> dateFinder(){
-        ArrayList <Integer> last7Days = new ArrayList<>();
-        String date = store.getDateInFocus();
-        String Day = date.split("-")[2];
-        int dayValue = Integer.parseInt(Day);
-        last7Days.add(dayValue);
-        for(int i = 1; i <7; i++){
-            if(dayValue>=7) {
-                int toAdd = dayValue - i;
-                last7Days.add(toAdd);
-                Log.i("toAdd", String.valueOf(toAdd));
+        public static ArrayList<Integer> dateFinder() {
+            ArrayList<Integer> last7Days = new ArrayList<>();
+            String date = store.getDateInFocus();
+            String[] parts = date.split("-");
+            int year = Integer.parseInt(parts[0]);
+            int month = Integer.parseInt(parts[1]);
+            int day = Integer.parseInt(parts[2]);
+
+            // If the current day is less than 7, determine the remaining days of the previous month
+            if (day < 7) {
+                // Calculate the last day of the previous month
+                int lastDayOfPrevMonth = LocalDate.of(year, month, 1).minusDays(1).getDayOfMonth();
+                int remainingDays = 7 - day;
+
+                for (int i = day; i > 0; i--) {
+                    last7Days.add(i);
+                }
+
+                // Add remaining days from the end of the last month in descending order
+                for (int i = lastDayOfPrevMonth; i > lastDayOfPrevMonth - remainingDays; i--) {
+                    last7Days.add(i);
+                }
             }
+            if(day>=7) {
+                for (int i = day; i > 0; i--) {
+                    last7Days.add(i);
+                }
+            }
+
+            return last7Days;
         }
 
-        return last7Days;
-
-    }
 }
