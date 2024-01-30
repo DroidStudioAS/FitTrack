3ea9f069f4aef71925600767284d5fccd8d7cb12
DroidStudioAS <as.droiddevteam@gmail.com>
2024-01-30 08:05:15 +0100

    ready to connect network logic to the EditGoalsDialog

--- app/src/main/java/com/aa/fittracker/dialog/EditGoalsDialog.java
+++ app/src/main/java/com/aa/fittracker/dialog/EditGoalsDialog.java
@@ -3,6 +3,9 @@ package com.aa.fittracker.dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +17,10 @@ public class EditGoalsDialog extends Dialog {
 
     TextView usernameTvEgd;
 
+    EditText newWeightGoalEt;
+
+    ImageView closeDialogButton;
+
     public EditGoalsDialog(@NonNull Context context) {
         super(context);
     }
@@ -22,10 +29,26 @@ public class EditGoalsDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_goals_dialog);
-
+        /*********UI initializations**********/
         usernameTvEgd=(TextView) findViewById(R.id.usernameDialogLabel);
+        newWeightGoalEt=(EditText) findViewById(R.id.newWeightGoalEt);
+        closeDialogButton=(ImageView)findViewById(R.id.closeButton);
 
+        /*********End  Of UI initializations**********/
+        //set the username for the user
         usernameTvEgd.setText(store.getUSERNAME());
+        //set the ET to the old goal
+        newWeightGoalEt.setText(store.getUserWeightKg());
+
+        /*************OnClicklisteners*****************/
+        closeDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        /*************End Of OnClicklisteners*****************/
 
     }
 }
