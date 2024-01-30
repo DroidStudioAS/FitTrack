cb558239ffa3cdb6a356894d7a13a811aae40b19
DroidStudioAS <as.droiddevteam@gmail.com>
2024-01-30 14:22:31 +0100

    added all the initializations for ui elemetns in the freestyle input dialog

--- app/src/main/java/com/aa/fittracker/dialog/FreestyleInputDialog.java
+++ app/src/main/java/com/aa/fittracker/dialog/FreestyleInputDialog.java
@@ -2,8 +2,10 @@ package com.aa.fittracker.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.media.Image;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -15,12 +17,31 @@ import com.aa.fittracker.R;
 public class FreestyleInputDialog extends Dialog {
 
     ImageView closeFid;
+    ImageView fiConfirmTrigger;
+
+    Button fiEazyBut;
+    Button fiMediumBut;
+    Button fiHardBut;
+
+    EditText fiTrainingDescriptionEt;
+
     public FreestyleInputDialog(@NonNull Context context) {
         super(context);
         setContentView(R.layout.freestyle_input_layout);
 
         /*********Ui Initializations**********/
         closeFid=(ImageView) findViewById(R.id.closeFid);
+        fiConfirmTrigger=(ImageView)findViewById(R.id.fInputTrigger);
+
+        fiEazyBut=(Button)findViewById(R.id.fiEazyBut);
+        fiMediumBut=(Button)findViewById(R.id.fiMidBut);
+        fiHardBut=(Button)findViewById(R.id.fiHardBut);
+
+        fiTrainingDescriptionEt=(EditText)findViewById(R.id.fiTrainingDescEt);
+
+
+
+
 
         /*********End Of Ui Initializations**********/
 
