52ad18abf0ba62102c548cc9261836100c8af2e6
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-03 18:38:05 +0100

    added infoDialog for journal (fitness)

--- app/src/main/java/com/aa/fittracker/dialog/InfoDialog.java
+++ app/src/main/java/com/aa/fittracker/dialog/InfoDialog.java
@@ -17,20 +17,15 @@ import com.aa.fittracker.R;
 import org.w3c.dom.Text;
 
 public class InfoDialog extends Dialog {
-    String openedFrom = "";
 
-    TextView infoTitle;
-    TextView trainingExp;
-    TextView tJournalExp;
-    TextView wJournalExp;
+    TextView journalBut;
+    TextView comButton;
+    TextView trButton;
+    TextView fitInfoButton;
 
-    TextView expTarget;
+    Group journalSubMenu;
+    Group fitJournal;
 
-    Group trainingGroup;
-    Group tjGroup;
-    Group wjGroup;
-
-    ImageView diss;
 
 
     public InfoDialog(@NonNull Context context) {
@@ -38,7 +33,7 @@ public class InfoDialog extends Dialog {
     }
 
     public void setOpenedFrom(String openedFrom) {
-        this.openedFrom = openedFrom;
+
     }
 
     @Override
@@ -46,57 +41,34 @@ public class InfoDialog extends Dialog {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_info);
 
-        trainingExp = (TextView) findViewById(R.id.trainingsExpButton);
-        tJournalExp = (TextView) findViewById(R.id.trainingJournalExpButton);
-        wJournalExp = (TextView) findViewById(R.id.weightJournalExpButton);
+        journalBut=(TextView) findViewById(R.id.journalInfo);
+        fitInfoButton = (TextView)findViewById(R.id.fitInfo);
 
-        trainingGroup = (Group) findViewById(R.id.trainingGroup);
-        tjGroup = (Group) findViewById(R.id.tjGroup);
-        wjGroup = (Group) findViewById(R.id.wjGroup);
+        journalSubMenu=(Group) findViewById(R.id.journalSubMenu);
 
-        expTarget = (TextView) findViewById(R.id.expTarget);
-        diss=(ImageView)findViewById(R.id.diss);
-        diss.setOnClickListener(new View.OnClickListener() {
+        fitJournal = (Group) findViewById(R.id.fitJournal);
+
+        journalBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dismiss();
+                journalSubMenu.setVisibility(View.VISIBLE);
+
+            }
+        });
+        fitInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fitJournal.setVisibility(View.VISIBLE);
             }
         });
 
 
-        trainingExp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openedFrom = "T";
-                focusOnTitle();
-                setExp();
-                trainingGroup.setVisibility(View.VISIBLE);
-                tjGroup.setVisibility(View.INVISIBLE);
-                wjGroup.setVisibility(View.INVISIBLE);
-            }
-        });
-        tJournalExp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openedFrom = "TJ";
-                focusOnTitle();
-                setExp();
-                trainingGroup.setVisibility(View.INVISIBLE);
-                tjGroup.setVisibility(View.VISIBLE);
-                wjGroup.setVisibility(View.INVISIBLE);
-            }
-        });
-        wJournalExp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openedFrom = "WJ";
-                focusOnTitle();
-                setExp();
-                trainingGroup.setVisibility(View.INVISIBLE);
-                tjGroup.setVisibility(View.INVISIBLE);
-                wjGroup.setVisibility(View.VISIBLE);
-            }
-        });
+
+
+
+
+
+
 
     }
 
@@ -105,57 +77,12 @@ public class InfoDialog extends Dialog {
     }
 
     public void setExp() {
-        switch (openedFrom) {
-            case "T":
-                expTarget.setText("The Training Page Is Where You Can Add You Training Routines.\n\nTo Add A Training Just Go To Add Trainings And Fill The Necessary data.\n\nIn The App The Following Colors, Represent The Routine Difficulty :");
-                break;
-            case "TJ":
-                expTarget.setText("The Training Journal Is Where You Can Log Your Exercise History.\n\nJust Pick A Date, And Tell The App What You Did On That Day!.");
-                break;
-            case "WJ":
-                expTarget.setText("The Weight Journal Is Going To Follow Your Weight For You.\n\nJust Make Sure To Enter Your Weight On The Day, And The App Will Do The Rest!.");
-                break;
-        }
+
     }
 
-    public void focusOnTitle()
+    public void focusOnTitle(){
 
-    {
-        switch (openedFrom) {
-            case "T":
-                trainingExp.setTextSize(20);
-                trainingExp.setTypeface(null, Typeface.BOLD);
 
-                tJournalExp.setTextSize(16);
-                tJournalExp.setTypeface(null, Typeface.NORMAL);
-
-                wJournalExp.setTextSize(16);
-                wJournalExp.setTypeface(null, Typeface.NORMAL);
-
-                break;
-            case "TJ":
-                trainingExp.setTextSize(16);
-                trainingExp.setTypeface(null, Typeface.NORMAL);
-
-                tJournalExp.setTextSize(20);
-                tJournalExp.setTypeface(null, Typeface.BOLD);
-
-                wJournalExp.setTextSize(16);
-                wJournalExp.setTypeface(null, Typeface.NORMAL);
-
-                break;
-            case "WJ":
-                trainingExp.setTextSize(16);
-                trainingExp.setTypeface(null, Typeface.NORMAL);
-
-                tJournalExp.setTextSize(16);
-                tJournalExp.setTypeface(null, Typeface.NORMAL);
-
-                wJournalExp.setTextSize(20);
-                wJournalExp.setTypeface(null, Typeface.BOLD);
-
-                break;
-        }
     }
 }
 
